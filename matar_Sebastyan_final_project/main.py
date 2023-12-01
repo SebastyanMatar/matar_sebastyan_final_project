dda8eaef98c0646bbe0ca9c9b633598470992c9f
SebastyanMatar <142934953+SebastyanMatar@users.noreply.github.com>
2023-12-01 12:47:47 -0800

    final project so far

---
+++ matar_Sebastyan_final_project/main.py
@@ -0,0 +1,5 @@
+#This file was created by Sebastyan Matar
+#I am really interested in video game creation and I really enjoyed making the last video game for this class.
+#It will be titled: Gladiator 
+# I am to include a final boss and have enemies that transform into a final boss. 
+#A goal i have is when you eliminate all the enemy mobs the final boss spawns.
